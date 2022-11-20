1c9c0301ba22902390f74f1d766d5d2b1ae64dc1
nevernotmove <nevernotmove@gmail.com>
2022-11-20 01:18:05 +0100

    Turn Chunk into class

--- src/parser/Chunk.ts
+++ src/parser/Chunk.ts
@@ -1,6 +1,13 @@
-export type Chunk = {
-	header: string, 
-	chord: string,
-	text: string,
-	lineId: number
+export class Chunk {
+	header: string;
+	chord: string;
+	text: string;
+	lineId: number;
+
+	constructor(header: string, chord: string, text: string, lineId: number) {
+		this.header = header;
+		this.chord = chord;
+		this.text = text;
+		this.lineId = lineId;
+	}
 }
