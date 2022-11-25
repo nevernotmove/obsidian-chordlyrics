5a7c8aa8096ce80dbdb0476a8dbb6473395dbb20
nevernotmove <nevernotmove@gmail.com>
2022-11-25 03:05:18 +0100

    Refactor

--- src/chunk/getChordWithTextChunks.ts
+++ src/chunk/getChordWithTextChunks.ts
@@ -2,44 +2,50 @@ import {Chunk} from "./Chunk";
 import {ChunkType} from "./ChunkType";
 
 export default function getChordWithTextChunks(chordLine: string, textLine: string): Chunk[] {
+	[chordLine, textLine] = matchLengthWithWhitespace(chordLine, textLine);
+	return getChunksForSameLengthLines(chordLine, textLine);
+}
+
+function getChunksForSameLengthLines(line1: string, line2: string) {
+	return cutChunks(line1, getWordStartIndices(line1), line2, getWordStartIndices(line2));
+}
+
+function getWordStartIndices(text: string): number[] {
+	const indices: number[] = [];
+	for (const word of text.matchAll(/\S+/g)) {
+		if (word.index != null) indices.push(word.index);	
+	}
+	return indices;
+}
+
+function cutChunks(line1: string, line1Indices: number[], line2: string, line2Indices: number[]) {
 	const chunks: Chunk[] = [];
-	[chordLine, textLine] = matchLengths(chordLine, textLine);
-	const chordMatches = chordLine.matchAll(/\S+/g);
-	const wordMatches = textLine.matchAll(/\S+/g);
-	let chordsLeft = true;
-	let wordsLeft = true;
 	let lastCut = 0;
-	let chordIndex = chordMatches.next().value.index;
-	let wordIndex = wordMatches.next().value.index;
-	while (chordsLeft || wordsLeft) {
-		if (chordsLeft && (chordIndex <= wordIndex || !wordsLeft)) {
-			if (cutPossible(textLine, chordIndex)) {
-				chunks.push(cut(lastCut, chordIndex, chordLine, textLine));
-				lastCut = chordIndex;
+	let index1 = line1Indices.shift();
+	let index2 = line2Indices.shift();
+	while (index1 != undefined || index2 != undefined) {
+		if (index1 != undefined && (index2 == undefined || index1 <= index2)) {
+			if (cutPossible(line2, index1)) {
+				chunks.push(cut(lastCut, index1, line1, line2));
+				lastCut = index1;
 			}
-			const chord = chordMatches.next().value;
-			chord ? chordIndex = chord.index : chordsLeft = false;
-		} else if (wordsLeft) {
-			if (cutPossible(chordLine, wordIndex)) {
-				chunks.push(cut(lastCut, wordIndex, chordLine, textLine));
-				lastCut = wordIndex;
+			index1 = line1Indices.shift()!;
+		} else if (index2 != undefined) {
+			if (cutPossible(line1, index2)) {
+				chunks.push(cut(lastCut, index2, line1, line2));
+				lastCut = index2;
 			}
-			const word = wordMatches.next().value;
-			word ? wordIndex = word.index : wordsLeft = false;
+			index2 = line2Indices.shift()!;
 		}
 	}
-	chunks.push(cut(lastCut, chordLine.length, chordLine, textLine));
+	chunks.push(cut(lastCut, line1.length, line1, line2));
 	return chunks;
 }
 
-function matchLengths(chordLine: string, textLine: string)  {
-	if (chordLine.length > textLine.length) {
-		const diff = chordLine.length - textLine.length;
-		textLine += " ".repeat(diff);
-	} else if (textLine.length > chordLine.length) {
-		const diff = textLine.length - chordLine.length;
-		chordLine += " ".repeat(diff);
-	}
+function matchLengthWithWhitespace(chordLine: string, textLine: string)  {
+	const diff = Math.abs(chordLine.length - textLine.length);
+	if (chordLine.length > textLine.length) textLine += " ".repeat(diff);
+	else chordLine += " ".repeat(diff);
 	return [chordLine, textLine];
 }
 
