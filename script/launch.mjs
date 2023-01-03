1456e8e21d21eff3468adcfa2ba838965bce7496
nevernotmove <nevernotmove@gmail.com>
2023-01-03 04:02:50 +0100

    Add ability to open file in obsidian vault via script

--- script/launch.mjs
+++ script/launch.mjs
@@ -1,5 +1,5 @@
 import open from "open";
 
-// TODO Create test vault, copy build to test vault, open test vault
-// TODO Create and open test vault
-await open('obsidian://ac');
+// TODO Copy build to test vault
+// TODO Check out hot reload plugin
+await open('obsidian://open?vault=test-vault&file=chord-lyrics');
